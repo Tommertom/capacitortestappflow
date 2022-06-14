be7ec85edfc49370ef079ef1736fb75fa6b5a951
Tommertom <Tom.Gruintjes@gmail.com>
2022-06-15 00:16:35 +0200

    Finamiga test

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -7,7 +7,7 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1 VIRU</IonTitle>
+          <IonTitle>Tab 1 Finamiga</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
